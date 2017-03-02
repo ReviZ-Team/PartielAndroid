b7610b61aabfd4f5d53a14b17176f0bf6653978c
mohamed <momo-du_78@hotmail.fr>
2017-03-03 00:49:49 +0100

    ProjetAsyncTask

---
+++ AsyncTask/app/build/generated/source/buildConfig/debug/progmobile/semestre2/master2/miage/mohamed/projetasynctask/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package progmobile.semestre2.master2.miage.mohamed.projetasynctask;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "progmobile.semestre2.master2.miage.mohamed.projetasynctask";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
